765b2a9c16604418e2c76f85b1d52712b23e8d4d
Homtst <112351544+Homtst@users.noreply.github.com>
2022-08-30 17:36:50 -0600

    Finished Variable Assigment

---
+++ Variables.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+     int houses = 10;
+     Console.WriteLine(houses);
+     int neighbors = 27;
+     Console.WriteLine(neighbors);
+     char randomLetter = 'i';
+     Console.WriteLine(randomLetter);
+     char firstLetter = 'a';
+     Console.WriteLine(firstLetter);
+     string nameofDog = "Huskey";
+     Console.WriteLine(nameofDog);
+     sting nameofCat = "Ragdoll";
+     Console.WriteLine(nameofCat);
+     bool wearingaHat = false;
+     Console.WriteLine(wearingaHat);
+     bool wearingsomeSocks = True;
+     Console.WriteLine(wearingsomeSocks);
+     double change = 5.68d;
+     Console.WriteLine(change);
+     float piecesofPie = 3.50f;
+     Console.WriteLine(piecesofPie);
+    }
+  }
+}
